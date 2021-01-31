02a0dee28b91c40c0728a74b5a27629471025a25
sti320a <sti320a@gmail.com>
2021-01-31 16:12:31 +0900

    Tempral fix for app

--- app.py
+++ app.py
@@ -1,16 +1,52 @@
-import asyncio
-import websockets
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
 clients = []
 
 
-async def echo(websocket, path):
-    # TODO: client情報によって処理を分ける
-    async for message in websocket:
-        await websocket.send(message)
-        print(message)
+class SimpleEcho(WebSocket):
 
-start_server = websockets.serve(echo, 'localhost', 8765)
+    def handleMessage(self):
+        for client in clients:
+            if client != self:  # judge
+                print('-----on----')
+                print(self.data)
+                client.sendMessage(self.data)
+            else:
+                print('----skip-----')
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+    def handleConnected(self):
+        print(self.address, 'connected')
+        clients.append(self)  # add client
+
+    def handleClose(self):
+        print(self.address, 'closed')
+
+
+server = SimpleWebSocketServer('', 8765, SimpleEcho)
+server.serveforever()
+
+
+# import asyncio
+# import websockets
+
+# clients = set()
+
+
+# async def register_client(websocket):
+#     clients.add(websocket)
+
+
+# async def echo(websocket, path):
+#     # TODO: client情報によって処理を分ける
+#     # 1. 初回接続時にclientを一意に識別できるように記録する
+#     # 2. clientが既に登録済みの場合、メッセージを返さない
+#     async for message in websocket:
+#         if websocket not in clients:
+#             await websocket.send(message)
+#             await register_client(websocket)
+#             # print(message)
+
+# start_server = websockets.serve(echo, 'localhost', 8765)
+
+# asyncio.get_event_loop().run_until_complete(start_server)
+# asyncio.get_event_loop().run_forever()
